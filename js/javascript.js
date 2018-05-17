a32861105c9bcba4339458fda6ed6b4064e5cffc
Laura Alias <laura.alias.email@gmail.com>
2018-05-17 16:15:50 +0100

    jquery menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,35 @@
+/* Book Leaf Suggestion Page Click Me */
+
 $(document).ready(function(){
 	$("input.bookleaf").click(function(){
 	$("#text").fadeToggle("slow");
    	 	});
-   	 });
-   	 
+	});
+	
+/* Show More Menu Page Button Left */
+	
+$(document).ready(function(){
+    $("button.button1").click(function(){
+        $("div.animated1").animate({
+            top: '26vh',
+            left: '54vh',
+            opacity: '0.5',
+            height: '30vh',
+            width: '40%'
+        });
+    });
+});
+
+/* Show More Menu Page Button Right */
+
+$(document).ready(function(){
+    $("button.button2").click(function(){
+        $("div.animated2").animate({
+            top: '56vh',
+            left: '54vh',
+            opacity: '0.5',
+            height: '30vh',
+            width: '40%'
+        });
+    });
+});
