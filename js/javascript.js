f8c1b2ed92f20e5c1a720c4c2d9f5e4bd4641d5c
Laura Alias <laura.alias.email@gmail.com>
2018-05-23 01:45:37 +0100

    tablet format

--- js/javascript.js
+++ js/javascript.js
@@ -33,3 +33,183 @@ $(document).ready(function(){
         });
     });
 });
+
+/* Rectangle left */
+
+$(document).ready(function(){
+    $("input.rectangle1").click(function(){
+        $("div.rectangletext1").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle2").click(function(){
+        $("div.rectangletext2").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle3").click(function(){
+        $("div.rectangletext3").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle4").click(function(){
+        $("div.rectangletext4").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle5").click(function(){
+        $("div.rectangletext5").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle6").click(function(){
+        $("div.rectangletext6").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle7").click(function(){
+        $("div.rectangletext7").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle8").click(function(){
+        $("div.rectangletext8").animate({
+            top: '26vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+/* Rectangle right */
+
+$(document).ready(function(){
+    $("input.rectangle9").click(function(){
+        $("div.rectangletext9").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle10").click(function(){
+        $("div.rectangletext10").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle11").click(function(){
+        $("div.rectangletext11").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle12").click(function(){
+        $("div.rectangletext12").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle13").click(function(){
+        $("div.rectangletext13").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle14").click(function(){
+        $("div.rectangletext14").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle15").click(function(){
+        $("div.rectangletext15").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("input.rectangle16").click(function(){
+        $("div.rectangletext16").animate({
+            top: '56vh',
+            left: '54vh',
+            height: '30vh',
+            width: '45%'
+        });
+    });
+});
